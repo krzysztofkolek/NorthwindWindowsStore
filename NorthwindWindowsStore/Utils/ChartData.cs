c586deaaaca90e991e329c000560835f0b0a9afa
krzysztofkolek <krzysztof.kolek.praca@gmail.com>
2016-07-26 23:23:25 +0200

    Employee chart Test chart data model

---
+++ NorthwindWindowsStore/Utils/ChartData.cs
@@ -0,0 +1,17 @@
+﻿namespace NorthwindWindowsStore.Utils
+{
+    public class ChartData
+    {
+        public string Label
+        {
+            get;
+            set;
+        }
+
+        public double Value
+        {
+            get;
+            set;
+        }
+    }
+}
