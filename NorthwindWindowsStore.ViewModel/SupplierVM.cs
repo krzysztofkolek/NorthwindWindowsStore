afb54c928f584928e1fb48a12f7372b221d4d204
krzysztofkolek <krzysztof.kolek.praca@gmail.com>
2016-07-29 15:55:45 +0200

    Suppliers Draft + Added a DataGrid with a SuppliersViewModel + Added a SuppliersVM
    * Change the service endpoint to minimize the data transfer overhead.

---
+++ NorthwindWindowsStore.ViewModel/SupplierVM.cs
@@ -0,0 +1,48 @@
+﻿namespace NorthwindWindowsStore.ViewModel
+{
+    public class SupplierVM : BaseVM<SupplierViewModel>
+    {
+        public SupplierVM()
+        {
+        }
+
+        public SupplierVM(SupplierViewModel model)
+        {
+            Member.SupplierID = model.SupplierID;
+            Member.CompanyName = model.CompanyName;
+            Member.ContactName = model.ContactName;
+            Member.ContactTitle = model.ContactTitle;
+            Member.Address = model.Address;
+            Member.City = model.City;
+            Member.Region = model.Region;
+            Member.PostalCode = model.PostalCode;
+            Member.Country = model.Country;
+            Member.Phone = model.Phone;
+            Member.Fax = model.Fax;
+            Member.HomePage = model.HomePage;
+        }
+
+
+        public string ComapnyName
+        {
+            get
+            {
+                return Member.CompanyName;
+            }
+        }
+
+        public string Contact
+        {
+            get
+            {
+                return Member.ContactName;
+            }
+        }
+
+        public string Address
+        {
+            get { return Member.Address; }
+        }
+
+    }
+}
--- NorthwindWindowsStore/Views/SuppliersMain.xaml.cs
+++ NorthwindWindowsStore/Views/SuppliersMain.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
+﻿using NorthwindWindowsStore.ViewModel;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+using Telerik.UI.Xaml.Controls.Grid;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,9 +12,14 @@ namespace NorthwindWindowsStore.Views
     /// </summary>
     public sealed partial class SuppliersMain : Page
     {
+        private List<SupplierVM> _suppliers = NorthwindWindowsStore.Utils.ContentManager.GetContent<SupplierViewModel, SupplierVM>("Suppliers/GetAll").Result;
+
         public SuppliersMain()
         {
             this.InitializeComponent();
+
+            RadDataGrid grid = this.FindName("SuppliersGrid") as RadDataGrid;
+            grid.ItemsSource = _suppliers;
         }
     }
 }
