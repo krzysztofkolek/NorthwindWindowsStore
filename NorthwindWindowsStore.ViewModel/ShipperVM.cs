f455333b153b3cd3376f744464290224356b4f96
krzysztofkolek <krzysztof.kolek.praca@gmail.com>
2016-07-30 23:52:45 +0200

    Shipper main page + Added VM model + Basic shipper page

---
+++ NorthwindWindowsStore.ViewModel/ShipperVM.cs
@@ -0,0 +1,31 @@
+﻿namespace NorthwindWindowsStore.ViewModel
+{
+    public class ShipperVM : BaseVM<ShipperViewModel>
+    {
+        public ShipperVM()
+        {
+        }
+
+        public ShipperVM(ShipperViewModel model)
+        {
+            Member.ShipperID = model.ShipperID;
+            Member.CompanyName = model.CompanyName;
+            Member.Phone = model.Phone;
+        }
+
+        public string CompanyName
+        {
+            get
+            {
+                return Member.CompanyName;
+            }
+        }
+        public string Phone
+        {
+            get
+            {
+                return Member.Phone;
+            }
+        }
+    }
+}
--- NorthwindWindowsStore/Views/ShippersMain.xaml.cs
+++ NorthwindWindowsStore/Views/ShippersMain.xaml.cs
@@ -1,30 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
-
-namespace NorthwindWindowsStore.Views
+﻿namespace NorthwindWindowsStore.Views
 {
+    using NorthwindWindowsStore.ViewModel;
+    using System.Collections.Generic;
+    using Telerik.UI.Xaml.Controls.Grid;
+    using Windows.UI.Xaml.Controls;
+
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// Shippers Main page
     /// </summary>
     public sealed partial class ShippersMain : Page
     {
+        private List<ShipperVM> _suppliers = NorthwindWindowsStore.Utils.ContentManager.GetContent<ShipperViewModel, ShipperVM>("Shippers/GetAll").Result;
+
         public ShippersMain()
         {
             this.InitializeComponent();
+
+            RadDataGrid grid = this.FindName("ShippersGrid") as RadDataGrid;
+            grid.ItemsSource = _suppliers;
         }
     }
 }
