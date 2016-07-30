46d5a38961e1f71ead2af8b654a766f508e148fe
krzysztofkolek <krzysztof.kolek.praca@gmail.com>
2016-07-31 00:15:21 +0200

    Products Min Page + Added ProductsVM

---
+++ NorthwindWindowsStore.ViewModel/ProductVM.cs
@@ -0,0 +1,33 @@
+﻿
+namespace NorthwindWindowsStore.ViewModel
+{
+
+    public class ProductVM : BaseVM<ProductViewModel>
+    {
+        public ProductVM()
+        { }
+
+        public ProductVM(ProductViewModel model)
+        {
+            Member.ProductID = model.ProductID;
+            Member.ProductName = model.ProductName;
+            Member.SupplierID = model.SupplierID;
+            Member.CategoryID = model.CategoryID;
+            Member.QuantityPerUnit = model.QuantityPerUnit;
+            Member.UnitPrice = model.UnitPrice;
+            Member.UnitsInStock = model.UnitsInStock;
+            Member.UnitsOnOrder = model.UnitsOnOrder;
+            Member.ReorderLevel = model.ReorderLevel;
+            Member.Discontinued = model.Discontinued;
+
+        }
+
+        public string ProductName
+        {
+            get
+            {
+                return Member.ProductName;
+            }
+        }
+    }
+}
--- NorthwindWindowsStore/Views/ProductsMain.xaml.cs
+++ NorthwindWindowsStore/Views/ProductsMain.xaml.cs
@@ -1,30 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
-
-namespace NorthwindWindowsStore.Views
+﻿namespace NorthwindWindowsStore.Views
 {
+    using NorthwindWindowsStore.ViewModel;
+    using System.Collections.Generic;
+    using Telerik.UI.Xaml.Controls.Grid;
+    using Windows.UI.Xaml.Controls;
+
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// 
     /// </summary>
     public sealed partial class ProductsMain : Page
     {
+        private List<ProductVM> _products = NorthwindWindowsStore.Utils.ContentManager.GetContent<ProductViewModel, ProductVM>("Products/GetAll").Result;
+
         public ProductsMain()
         {
             this.InitializeComponent();
+
+            RadDataGrid grid = this.FindName("ProductsGrid") as RadDataGrid;
+            grid.ItemsSource = _products;
         }
     }
 }
