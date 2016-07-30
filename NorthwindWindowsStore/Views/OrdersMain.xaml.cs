8c3e097fe981d467b3ba91d253f16af060d67f7c
krzysztofkolek <krzysztof.kolek.praca@gmail.com>
2016-07-31 01:07:46 +0200

    Orders Main Page + Added OrdersVM + Base orders functionality

--- NorthwindWindowsStore/Views/OrdersMain.xaml.cs
+++ NorthwindWindowsStore/Views/OrdersMain.xaml.cs
@@ -1,30 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
-
-namespace NorthwindWindowsStore.Views
+﻿namespace NorthwindWindowsStore.Views
 {
+    using NorthwindWindowsStore.ViewModel;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Telerik.UI.Xaml.Controls.Grid;
+    using Windows.UI.Xaml.Controls;
+
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// Orders Main Page
     /// </summary>
     public sealed partial class OrdersMain : Page
     {
+        private List<OrderVM> _orders = NorthwindWindowsStore.Utils.ContentManager.GetContent<OrderViewModel, OrderVM>("Orders/GetAll").Result;
+
         public OrdersMain()
         {
             this.InitializeComponent();
+            CheckData();
+
+            RadDataGrid grid = this.FindName("OrdersGrid") as RadDataGrid;
+            grid.ItemsSource = _orders;
+        }
+
+        private void CheckData()
+        {
+            _orders = (from item in _orders
+                       where item != null
+                       select item).ToList();
         }
     }
 }
